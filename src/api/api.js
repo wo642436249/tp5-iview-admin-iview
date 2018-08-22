3b71e81cfb8127e429662cf66633fb1ea3cf04ae
wo642436249 <642436249@qq.com>
2018-08-22 16:29:04 +0800

    Modify the Api file

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
-export const Host = 'http://****'
-export const FPM_HOST = 'http://www.dahuawulitou.xyz'
+export const Host = 'http://****' //基于swoole server的Api地址
+export const FPM_HOST = 'http://****' //非基于swoole的Api地址
 export const Login = Host + '/AdminLogin' //登陆
 export const AdminUser = Host + '/AdminUser' //获取、新增、更新、删除管理人员
 export const AdminRule = Host + '/AdminRule' //获取、新增、更新、删除后台菜单
