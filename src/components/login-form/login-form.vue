5024d406553c40ea03e59bbd70ffb6c6381ce495
wo642436249 <642436249@qq.com>
2018-08-22 16:17:06 +0800

    Modify the Introduce the file and login setup

--- src/components/login-form/login-form.vue
+++ src/components/login-form/login-form.vue
@@ -20,53 +20,53 @@
   </Form>
 </template>
 <script>
-  export default {
-    name: 'LoginForm',
-    props: {
-      userNameRules: {
-        type: Array,
-        default: () => {
-          return [
-            {required: true, message: '账号不能为空', trigger: 'blur'}
-          ]
+    export default {
+        name: 'LoginForm',
+        props: {
+            userNameRules: {
+                type: Array,
+                default: () => {
+                    return [
+                        {required: true, message: '账号不能为空', trigger: 'blur'}
+                    ]
+                }
+            },
+            passwordRules: {
+                type: Array,
+                default: () => {
+                    return [
+                        {required: true, message: '密码不能为空', trigger: 'blur'}
+                    ]
+                }
+            }
+        },
+        data() {
+            return {
+                form: {
+                    userName: 'admin123',
+                    password: 'admin123'
+                }
+            }
+        },
+        computed: {
+            rules() {
+                return {
+                    userName: this.userNameRules,
+                    password: this.passwordRules
+                }
+            }
+        },
+        methods: {
+            handleSubmit() {
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        this.$emit('on-success-valid', {
+                            userName: this.form.userName,
+                            password: this.form.password
+                        })
+                    }
+                })
+            }
         }
-      },
-      passwordRules: {
-        type: Array,
-        default: () => {
-          return [
-            {required: true, message: '密码不能为空', trigger: 'blur'}
-          ]
-        }
-      }
-    },
-    data () {
-      return {
-        form: {
-          userName: 'wo642436249',
-          password: 'xhc123456'
-        }
-      }
-    },
-    computed: {
-      rules () {
-        return {
-          userName: this.userNameRules,
-          password: this.passwordRules
-        }
-      }
-    },
-    methods: {
-      handleSubmit () {
-        this.$refs.loginForm.validate((valid) => {
-          if (valid) {
-            this.$emit('on-success-valid', {
-              userName: this.form.userName,
-              password: this.form.password
-            })
-          }
-        })
-      }
     }
-  }
 </script>
